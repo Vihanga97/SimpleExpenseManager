fb6aefb3ff3b52045def0ff51a5ecbb396f33c33
Vihanga97 <vihanga.dewmini.97@gmail.com>
2019-12-19 21:52:45 +0530

    Implemented persistent expense manager

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/control/PersistentExpenseManager.java
@@ -1,10 +1,27 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.control;
 
+import android.content.Context;
+
 import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.exception.ExpenseManagerException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.AccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.TransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentAccountDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.impl.PersistentTransactionDAO;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.database.DBHelper;
 
 public class PersistentExpenseManager extends ExpenseManager {
-    @Override
-    public void setup() throws ExpenseManagerException {
+    private DBHelper dbHelper;
 
+    public PersistentExpenseManager(Context context) {
+        dbHelper = new DBHelper(context);
+        setup();
+    }
+
+    @Override
+    public void setup() {
+        TransactionDAO persistentTransactionDAO = new PersistentTransactionDAO(dbHelper);
+        setTransactionsDAO(persistentTransactionDAO);
+        AccountDAO persistentAccountDAO = new PersistentAccountDAO(dbHelper);
+        setAccountsDAO(persistentAccountDAO);
     }
 }
