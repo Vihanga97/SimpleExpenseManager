df0d572f45ce5387513cf67863d7ee90dafc125f
Vihanga97 <vihanga.dewmini.97@gmail.com>
2019-12-19 20:52:46 +0530

    Implemented the database creation

--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/database/DBConstant.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/database/DBConstant.java
@@ -1,5 +1,20 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager.database;
 
 public class DBConstant {
+    public final static String TRANSACTION_ACCOUNT_NUM = "account_num";
+    public final static String ACCOUNT_TABLE_NAME = "account";
+    public final static String EXPENSE_TYPE_TABLE_NAME = "expense_type";
+    public final static String TRANSACTION_TABLE_NAME = "`transaction`";
+    public final static String TRANSACTION_ID = "id";
+    public final static String TRANSACTION_TYPE = "type";
+    public final static String TRANSACTION_DATE = "date";
+    public final static String TRANSACTION_AMOUNT = "amount";
+    public final static String ACCOUNT_ACCOUNT_NUM = "account_num";
+    public final static String ACCOUNT_BANK_NAME = "bank_name";
+    public final static String ACCOUNT_HOLDER_NAME = "holder_name";
+    public final static String ACCOUNT_BALANCE = "balance";
+    public final static String EXPENSE_TYPE_NAME = "name";
+    public final static String TYPE_OF_EXPENSE = "EXPENSE";
+    public final static String TYPE_OF_INCOME = "INCOME";
 
 }
--- app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/database/DBHelper.java
+++ app/src/main/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/database/DBHelper.java
@@ -11,9 +11,37 @@ public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context){
         super(context, DB_NAME, null, DB_VERSION);
     }
-
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " +
+                DBConstant.EXPENSE_TYPE_TABLE_NAME + "(" +
+                DBConstant.EXPENSE_TYPE_NAME + " VARCHAR(64) PRIMARY KEY " +
+                ");"
+        );
+
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " +
+                DBConstant.ACCOUNT_TABLE_NAME + "(" +
+                DBConstant.ACCOUNT_ACCOUNT_NUM + " VARCHAR PRIMARY KEY, " +
+                DBConstant.ACCOUNT_BANK_NAME + " VARCHAR NOT NULL, " +
+                DBConstant.ACCOUNT_HOLDER_NAME + " VARCHAR NOT NULL, " +
+                DBConstant.ACCOUNT_BALANCE + " DECIMAL(128,2) NOT NULL" +
+                ");"
+        );
+
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS " +
+                DBConstant.TRANSACTION_TABLE_NAME + "(" +
+                DBConstant.TRANSACTION_ID + " INTEGER PRIMARY KEY, " +
+                DBConstant.TRANSACTION_ACCOUNT_NUM + " VARCHAR NOT NULL, " +
+                DBConstant.TRANSACTION_TYPE + " VARCHAR NOT NULL, " +
+                DBConstant.TRANSACTION_DATE + " TIMESTAMP NOT NULL, " +
+                DBConstant.TRANSACTION_AMOUNT + " DECIMAL(128,2) NOT NULL, " +
+                " FOREIGN KEY (" + DBConstant.TRANSACTION_ACCOUNT_NUM + ") REFERENCES " +
+                DBConstant.ACCOUNT_TABLE_NAME + "(" + DBConstant.ACCOUNT_ACCOUNT_NUM + ")," +
+                " FOREIGN KEY (" + DBConstant.TRANSACTION_TYPE + ") REFERENCES " +
+                DBConstant.EXPENSE_TYPE_TABLE_NAME + "(" + DBConstant.EXPENSE_TYPE_NAME + ")" +
+                ");"
+        );
 
     }
 
